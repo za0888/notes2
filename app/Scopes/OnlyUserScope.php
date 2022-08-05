ecf1f6788dddcb2765e3525b53513dbe1aa5e74c
za0888 <56291618+za0888@users.noreply.github.com>
2022-08-05 21:25:51 +0300

    Global scope plus

--- app/Scopes/OnlyUserScope.php
+++ app/Scopes/OnlyUserScope.php
@@ -5,20 +5,23 @@ namespace App\Scopes;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
+
 class OnlyUserScope implements Scope
 
 {
     /**
      * Apply the scope to a given Eloquent query builder.
      *
-     * @param  \Illuminate\Database\Eloquent\Builder  $builder
-     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param \Illuminate\Database\Eloquent\Builder $builder
+     * @param \Illuminate\Database\Eloquent\Model $model
      * @return void
      */
 
     public function apply(Builder $builder, Model $model)
-{
-    $builder->where('created_by_user', auth()->id());
-}
+    {
+        if (!auth()->user()?->is_admin) {
+            $builder->where('created_by_user', auth()->id());
+        }
+    }
 
 }
